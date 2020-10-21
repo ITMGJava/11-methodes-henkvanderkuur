f9e454420358ec94475f33b72f08f1b5c7085c61
Henk van der Kuur <info@vanderkuur.com>
2020-10-21 15:02:20 +0200

    Methode naam

--- src/Methodes.java
+++ src/Methodes.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class Methodes {
+    public static void main(String[] args) {
+        helloWorld();
+        groet();
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Wat is je naam? ");
+        String naam = s.nextLine();
+        groet(naam);
+
+    }
+
+    static void helloWorld() {
+        System.out.println("Hello World");
+    }
+
+    static void groet() {
+            System.out.println("Hallo, User");
+    }
+
+    static void groet(String naam){
+        System.out.println("Dank je wel " +naam);
+
+
+    }
+
 }
+
